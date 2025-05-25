b460b3ed881493be2412a4c7bde8ab126cddf66e
Anumoy Murmu <103399542+AnumoyMurmu@users.noreply.github.com>
2025-05-26 01:48:54 +0530

    Update print_1toN_withoutLoop_NUMBERS.cpp

--- print_1toN_withoutLoop_NUMBERS.cpp
+++ print_1toN_withoutLoop_NUMBERS.cpp
@@ -1,1 +1,21 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
 
+void printnos(int n){
+    
+    if(n>0){
+        printnos(n-1);
+        cout<<n<<endl;
+    }
+    
+    return;
+}
+
+int main() {
+    // Write C++ code here
+
+    printnos(10);
+
+    return 0;
+}
